af0bd3c8463d051222df113e638b62e9210655a9
Mithikayl <yousufshahzadd@gmail.com>
2022-02-27 14:28:12 +0000

    Updates

---
+++ precache-manifest.47e1e7b7aa5605b46a9ce377623bfc0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ede2c85016937f6b8bf8",
+    "url": "/f1-data-app/static/css/main.bc84d8dd.chunk.css"
+  },
+  {
+    "revision": "ede2c85016937f6b8bf8",
+    "url": "/f1-data-app/static/js/main.ede2c850.chunk.js"
+  },
+  {
+    "revision": "a58ce88936aea7ba4cc1",
+    "url": "/f1-data-app/static/js/1.a58ce889.chunk.js"
+  },
+  {
+    "revision": "4ba122d120f96d0c8d52",
+    "url": "/f1-data-app/static/js/runtime~main.4ba122d1.js"
+  },
+  {
+    "revision": "ea4e4ee446011c99fc8d534fc4eb8d0d",
+    "url": "/f1-data-app/static/media/Formula1-Regular.ea4e4ee4.ttf"
+  },
+  {
+    "revision": "35fc5c37c4108b0ae21f449a8f3bb904",
+    "url": "/f1-data-app/index.html"
+  }
+];
