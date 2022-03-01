125a7e28fe0f2304d3ff3504a57d454e829c3def
Mithikayl <yousufshahzadd@gmail.com>
2022-03-01 19:38:59 +0000

    webpage much more structured now + watermark

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ class App extends React.Component {
     render() {
         return (
             <>
-            <h1 className="h">Formula 1 Race Finder</h1>
-            <div>
-                <RacedName></RacedName>
+            <div id="container">
+                <div id="header">
+                </div>
+                <div id="body">
+                    <h1 className="h">Formula 1 Race Finder</h1>
+                    <RacedName></RacedName>
+                </div>
+                <div id="footer">
+                    <p className="footerText">made by <a href="https://github.com/mithikayl" target="_blank" without rel="noopener noreferrer" className="githubLink">mithikayl</a></p>
+                </div>
             </div>
             </>
         )
