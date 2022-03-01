9a3ebecedfb9293a90de4cb2b869e78065e8b6c1
Mithikayl <yousufshahzadd@gmail.com>
2022-03-01 19:38:39 +0000

    removed testing code

--- src/f1.js
+++ src/f1.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 export function getData(year) {
     const url = `https://ergast.com/api/f1/${year}.json`;
     const promise = axios.get(url);
-    console.log(promise);
     const dataPromise = promise.then((response) => response.data);
     return dataPromise;
 }
